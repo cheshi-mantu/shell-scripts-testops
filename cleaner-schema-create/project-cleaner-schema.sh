a23812a2c522381dfcb164cb7e2c03979918a8d5
Egor Ivanov <cheshi.mantu@pm.me>
2022-12-29 19:48:13 +0300

    added per project cleaner schema and updated the global

--- cleaner-schema-create/project-cleaner-schema.sh
+++ cleaner-schema-create/project-cleaner-schema.sh
@@ -11,7 +11,7 @@ for ARTEFACT in $TARGET_ARTEFACT
     do
         for STATUS in $TEST_STATUS
         do 
-            curl -X POST "${ALLURE_ENDPOINT}/api/rs/cleanerschema" --header "accept: */*" --header "Content-Type: application/json" --header "Authorization: Api-Token ${ALLURE_TOKEN}" -d "{\"status\": \"${STATUS}\",\"target\": \"${ARTEFACT}\",\"delay\": ${DELETE_DELAY}}"
+            curl -X POST "${ALLURE_ENDPOINT}/api/rs/cleanerschema" --header "accept: */*" --header "Content-Type: application/json" --header "Authorization: Api-Token ${ALLURE_TOKEN}" -d "{\"projectId\": ${ALLURE_PROJECT_ID},\"status\": \"${STATUS}\",\"target\": \"${ARTEFACT}\",\"delay\": ${DELETE_DELAY}}"
         done
     done
 
