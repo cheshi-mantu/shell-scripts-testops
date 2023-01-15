3f0ff95b5c9c159b27c63e82b6dc7160df757d55
Egor Ivanov <cheshi.mantu@protonmail.com>
2023-01-15 14:55:27 +0300

    adding comments for the curl requests global

--- cleaner-schema-create/global-cleaner-schema.sh
+++ cleaner-schema-create/global-cleaner-schema.sh
@@ -12,9 +12,12 @@ for ARTEFACT in $TARGET_ARTEFACT
     do
         for STATUS in $TEST_STATUS
         do 
+            echo "Setting ${ARTEFACT} for ${STATUS} tests to expire after ${DELETE_DELAY} after creation \n"
             curl -X POST "${ALLURE_ENDPOINT}/api/rs/cleanerschema" --header "accept: */*" --header "Content-Type: application/json" --header "Authorization: Api-Token ${ALLURE_TOKEN}" -d "{\"status\": \"${STATUS}\",\"target\": \"${ARTEFACT}\",\"delay\": ${DELETE_DELAY}}"
+            echo "\n"
         done
     done
 
 
-
+# this will trigger the creation of the blob remove tasks immediately
+# curl -X POST "${ALLURE_ENDPOINT}/api/rs/cleanup/scheduler/cleaner_schema_global" --header "accept: */*" --header "Authorization: Api-Token ${ALLURE_TOKEN}" -d ''
