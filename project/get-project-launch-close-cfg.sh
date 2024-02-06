05ff01bde245b415ec75ff1975ec9daf7c23ec8c
Egor Ivanov <cheshi.mantu@protonmail.com>
2024-02-06 12:12:56 +0200

    comments
    Signed-off-by: Egor Ivanov <cheshi.mantu@protonmail.com>

--- project/get-project-launch-close-cfg.sh
+++ project/get-project-launch-close-cfg.sh
@@ -7,12 +7,14 @@ countPage=0
 lastPage=false
 FILE_PROJECTS_LIST=all-projects.txt
 FILE_PROJECTS_CLOSE=all-projects-close-rules.txt
-# Clean and go
+
+# Clear and go
 clear
 
 # echo "ALLURE_ENDPOINT: ${ALLURE_ENDPOINT}"
 # echo "ALLURE_TOKEN: ${ALLURE_TOKEN}"
 
+# saving some compute by using Bearer token
 JWT_TOKEN=$(curl -s -X POST "${ALLURE_ENDPOINT}/api/uaa/oauth/token" \
      --header "Expect:" \
      --header "Accept: application/json" \
@@ -21,11 +23,11 @@ JWT_TOKEN=$(curl -s -X POST "${ALLURE_ENDPOINT}/api/uaa/oauth/token" \
      --form "token=${ALLURE_TOKEN}" \
      | jq -r .access_token)
 
-
+# Clear prev runs
 rm ${FILE_PROJECTS_LIST}
 rm ${FILE_PROJECTS_CLOSE}
 
-
+# get projects
 while ! ${lastPage}; do
     echo "Getting page ${countPage}"
     RESPONSE=$(curl -X GET "${ALLURE_ENDPOINT}/api/rs/project?page=${countPage}&size=${pageSize}&sort=id%2CASC" --header "accept: */*" --header "Authorization: Bearer ${JWT_TOKEN}")
@@ -41,11 +43,14 @@ while ! ${lastPage}; do
     countPage=$((countPage + 1))
 done
 
+# save projects
 echo "Dumping the list of projects to file ${FILE_PROJECTS_LIST}"
 cat <<EOF > ${FILE_PROJECTS_LIST}
 ${PROJECTS}
 EOF
 
+
+# some socializing with the end user
 echo "Before we continue, please read this carefully"
 echo "#########################################################################################################"
 echo "Your list contains ${totalElements} elements, this means we are going to make ${totalElements} API calls"
@@ -54,11 +59,11 @@ echo "As an alternative you can edit ${FILE_PROJECTS_LIST} right now, keep only 
 
 read -p "Press Enter to continue..." name
 
+# load new projects list
 echo "Reading the list of projects from file ${FILE_PROJECTS_LIST}"
 PROJECTS=$(<${FILE_PROJECTS_LIST})
 
-echo
-
+# do stuff
 for PROJECT in ${PROJECTS}; do
     echo "${PROJECT}"
     RESPONSE=$(curl -X GET "${ALLURE_ENDPOINT}/api/rs/projectsettings/launchclose?projectId=${PROJECT}" --header "accept: */*" --header "Authorization: Bearer ${JWT_TOKEN}")
@@ -66,9 +71,10 @@ for PROJECT in ${PROJECTS}; do
 done
 
 echo "Dumping the rules for automatic launches closing to file ${FILE_PROJECTS_CLOSE}"
-
+# save the gathered data
 cat <<EOF > ${FILE_PROJECTS_CLOSE}
 ${FILE_DUMP}
 EOF
 clear
+# Ta-da!
 echo "Done!"
