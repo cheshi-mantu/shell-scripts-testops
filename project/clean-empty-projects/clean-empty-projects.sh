e04380191b38417d0ae1947193e70d0ce94a46e0
Egor I <egor.ivanov@pm.me>
2025-09-18 23:02:12 +0200

    cleaning empty projects Signed-off-by: Egor I <egor.ivanov@pm.me>

---
+++ project/clean-empty-projects/clean-empty-projects.sh
@@ -0,0 +1,60 @@
+# this could be a very long process
+# Variables
+ALLURE_TOKEN=$(cat ../secrets/token.txt)
+ALLURE_ENDPOINT=$(cat ../secrets/endpoint.txt)
+pageSize=2000
+countPage=0
+lastPage=false
+FILE_PROJECTS_LIST=all-projects.txt
+
+# Clear and go
+clear
+
+# echo "ALLURE_ENDPOINT: ${ALLURE_ENDPOINT}"
+# echo "ALLURE_TOKEN: ${ALLURE_TOKEN}"
+
+# saving some compute by using Bearer token
+JWT_TOKEN=$(curl -s -X POST "${ALLURE_ENDPOINT}/api/uaa/oauth/token" \
+     --header "Expect:" \
+     --header "Accept: application/json" \
+     --form "grant_type=apitoken" \
+     --form "scope=openid" \
+     --form "token=${ALLURE_TOKEN}" \
+     | jq -r .access_token)
+
+# Clear prev runs
+rm ${FILE_PROJECTS_LIST}
+rm ${FILE_PROJECTS_CLOSE}
+
+# # get projects
+# while ! ${lastPage}; do
+#     echo "Getting page ${countPage}"
+#     RESPONSE=$(curl -X GET "${ALLURE_ENDPOINT}/api/rs/project?page=${countPage}&size=${pageSize}&sort=id%2CASC" --header "accept: */*" --header "Authorization: Bearer ${JWT_TOKEN}")
+#     if [ ${countPage} = 0 ]; then
+#         PROJECTS="$(jq .content[].id <<< $RESPONSE)"
+#     else
+#         PROJECTS="${PROJECTS}\n$(jq .content[].id <<< $RESPONSE)"
+#     fi
+#     totalElements=$(jq .totalElements <<< $RESPONSE)
+#     lastPage=$(jq .last <<< $RESPONSE)
+#     echo "Page: ${countPage}"
+#     echo "Last page? - ${lastPage}"
+#     countPage=$((countPage + 1))
+# done
+
+# # save projects
+# echo "Dumping the list of projects to file ${FILE_PROJECTS_LIST}"
+# cat <<EOF > ${FILE_PROJECTS_LIST}
+# ${PROJECTS}
+# EOF
+
+# load new projects list
+echo "Reading the list of projects from file ${FILE_PROJECTS_LIST}"
+PROJECTS=$(<${FILE_PROJECTS_LIST})
+
+# do stuff
+for PROJECT in ${PROJECTS}; do
+    echo "${PROJECT}"
+    RESPONSE=$(curl -X GET "${ALLURE_ENDPOINT}/api/rs/projectsettings/launchclose?projectId=${PROJECT}" --header "accept: */*" --header "Authorization: Bearer ${JWT_TOKEN}")
+    FILE_DUMP="${FILE_DUMP}\n${RESPONSE}"
+done
