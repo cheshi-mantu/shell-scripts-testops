8fc2971923a0153b24f8cf18a547396b76f6734b
Egor I <egor.ivanov@pm.me>
2025-10-06 11:25:10 +0200

    dedicated get bearer
    Signed-off-by: Egor I <egor.ivanov@pm.me>

---
+++ auth-bearer/get-bearer-token.sh
@@ -0,0 +1,13 @@
+ALLURE_ENDPOINT=$1
+ALLURE_TOKEN=$2
+
+# saving some compute by using Bearer token
+JWT_TOKEN=$(curl -s -X POST "${ALLURE_ENDPOINT}/api/uaa/oauth/token" \
+     --header "Expect:" \
+     --header "Accept: application/json" \
+     --form "grant_type=apitoken" \
+     --form "scope=openid" \
+     --form "token=${ALLURE_TOKEN}" \
+     | jq -r .access_token)
+
+echo ${JWT_TOKEN}
