21616f457e197e90bf8c3eb125df17afef06a8fa
Egor I <egor.ivanov@pm.me>
2025-10-06 11:31:13 +0200

    adds bearer example
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- auth-bearer/get-bearer-token.sh
+++ auth-bearer/get-bearer-token.sh
@@ -1,6 +1,13 @@
 ALLURE_ENDPOINT=$1
 ALLURE_TOKEN=$2
 
+if [  -z "${ALLURE_TOKEN}" ]; then
+     echo "cannot start without ALLURE_TOKEN"
+     echo "Usage: $0 <ALLURE_ENDPOINT> <ALLURE_TOKEN>"
+     exit 1
+fi
+
+
 # saving some compute by using Bearer token
 JWT_TOKEN=$(curl -s -X POST "${ALLURE_ENDPOINT}/api/uaa/oauth/token" \
      --header "Expect:" \
--- test-cases/add-new-test-case.sh
+++ test-cases/add-new-test-case.sh
@@ -3,3 +3,21 @@ ALLURE_ENDPOINT=$(cat ../secrets/endpoint.txt)
 ALLURE_PROJECT_ID=$(cat ../secrets/project.id)
 
 clear
+
+BEARER_TOKEN=$(../auth-bearer/get-bearer-token.sh ${ALLURE_ENDPOINT} ${ALLURE_TOKEN})
+
+RESULT=$(curl -X GET "${ALLURE_ENDPOINT}/api/rs/testcase/deleted?projectId=${ALLURE_PROJECT_ID}&page=0&size=${RESTORE_TEST_CASES_PER_RUN}" --header "accept: */*" --header "Authorization: Api-Token ${ALLURE_TOKEN}")
+
+echo "Found deleted test cases in the project ${ALLURE_PROJECT_ID}": 
+
+IDS=$(echo $RESULT | jq .content[].id)
+
+echo ${IDS}
+
+
+for ID in ${IDS}
+do
+    echo "Restoring ${ID} \n"
+    curl -X PATCH ${ALLURE_ENDPOINT}/api/rs/testcase/${ID} --header "accept: */*" --header "Content-Type: application/json" --header "Authorization: Api-Token ${ALLURE_TOKEN}" -d "{\"deleted\": false}"
+    echo "\n"
+done
